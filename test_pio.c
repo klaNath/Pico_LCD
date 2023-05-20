dfd1cebfc551fbb197822ae3198e615a982c9e0d
Kazusa Okuda <kazusa@klamath.jp>
2023-05-21 06:20:47 +0900

    Initial Commit

---
+++ test_pio.c
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+
+//PIO Assemble 
+#include "test_pio.pio.h"
+
+int main() {
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    //ほとんどの命令メモリを使うため、Pico Wが使うPIOを避ける必要がある
+    PIO pio = pio0;
+
+    // PIOのメモリを格納する
+    uint offset_hsync = pio_add_program(pio, &ParallelTFT_hsync_clk_program);
+    uint offset_vsync = pio_add_program(pio, &ParallelTFT_vsync_program);
+    uint offset_data = pio_add_program(pio, &ParallelTFT_data_program);
+
+    // 空いているステートマシンを割り当て
+    uint sm_h = pio_claim_unused_sm(pio, true);
+    uint sm_v = pio_claim_unused_sm(pio, true);
+    uint sm_d = pio_claim_unused_sm(pio, true);
+
+    //先にData出力用SMを初期化し、横Pixel数-1の値を入れておく
+    ParallelTFT_data_program_init(pio, sm_d, offset_data, 4);
+    pio_sm_put_blocking(pio, sm_d, 399u);
+
+    ParallelTFT_hsync_clk_program_init(pio, sm_h, offset_hsync, 21, 22);
+    ParallelTFT_vsync_program_init(pio, sm_v, offset_vsync, 20);
+
+    //各SMのクロック分周器を同期させる。
+    pio_enable_sm_mask_in_sync(pio, (1u << sm_h) | (1u << sm_v)| (1u << sm_d));
+
+    while (true) {
+        // Blink
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+    }
+}
